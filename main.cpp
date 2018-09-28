0da5877b5085d2bdc6d3b10399f02b02fd38a72c
Frank Ngo <frank.ngo.1702@gmail.com>
2018-09-28 00:10:23 -0700

    Revert "split functions into a header file"
    This reverts commit 4722d4cfccf7d88e6bd9e58a323211a22c2781f2.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,37 @@
 /**
  * CPSC 362 VCS Project
  * Copyright(C) 2018 Team TBD
- * @author Josh Gomberg | jgomberg93@gmail.com
- * @author Michael Li | limichael1099419@gmail.com
- * @author Frank Ngo | frank.ngo@csu.fullerton.edu
- * @author Wellson Pan | dihydrogenmonoxide1337@gmail.com
+ * @author Josh Gomberg jgomberg93@gmail.com
+ * @author Michael Li limichael1099419@gmail.com
+ * @author Frank Ngo frank.ngo@csu.fullerton.edu
+ * @author Wellson Pan dihydrogenmonoxide1337@gmail.com
  *
  * main.cpp the main source code
 **/
 
-
-#include "Header.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <boost/filesystem.hpp>
 namespace fs = boost::filesystem;
 
-
+/**
+ * Find the total number of files within a folder and its sub-folders.
+ *
+ * @param string p path to the folder
+ * @return int the size of that folder
+**/
+int directorySize(std::string p)
+{
+	int i = 0;
+	for (fs::recursive_directory_iterator it(p); it != fs::recursive_directory_iterator(); ++it)
+	{
+		i++;
+	}
+	return i;
+}
 
 int main()
 {
